a76aa135c4e76cc2f0c6d84805ab17739aa3a16e
Sanki Coder <116650046+sankixcoder@users.noreply.github.com>
2023-01-11 15:50:08 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id = 24525445
 api_hash = "ec30d2a64a99aa17518c6506860dcf43"
 bot_token = "5758377234:AAH-xBcK8NTmWJOqbxqNwRDsVQtfX6RaYRg"
-auth_users = 5241758521 5604657476
+auth_users = 5241758521,5604657476
 groups = -1001642935380
