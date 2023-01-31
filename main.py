a9e27a1c628b125ccc2f0e8d5149d24fc787f1b9
niranjantechofficial <61946705+niranjantechofficial@users.noreply.github.com>
2023-01-31 13:00:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,6 @@ async def restart_handler(_, m):
 
 @bot.on_message(filters.command(["pro_txt"]))
 async def account_login(bot: Client, m: Message):
-    user = m.from_user.id if m.from_user is not None else None
     editable = await m.reply_text(
             "Hello Bruh **I am Text Downloader Bot**. I can download videos from **text** file one by one.**\n\nDeveloper** : NAAM TO SUNA HOGA**\nLanguage** : Python**\nFramework** : Pyrogram\n\nSend **TXT** File {Name : Link}")
     input: Message = await bot.listen(editable.chat.id)
