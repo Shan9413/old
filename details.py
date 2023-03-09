b24f5da8e11bd496d14135f2fbb8308e9b9867e5
DRAGO <88723798+Drago-Owner@users.noreply.github.com>
2023-03-09 22:05:44 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,4 @@
-api_id = 29702486
+api_id = "29702486"
 api_hash = "c036bb758301cf3d70a4cc34c7dc90e3"
 bot_token = "6256005508:AAEE-mJqSwMugxOC67tKy1OdonHPb8ThUdk"
-auth_users = 5795693351
-groups = -1001884733446
+sudo_group = -1001884733446
