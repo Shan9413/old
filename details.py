92c44b6642990eafee3fe831dd26ca80f437c9fd
ChVivekTomar <125326355+ChVivekTomar@users.noreply.github.com>
2023-03-11 02:07:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "29702486"
 api_hash = "c036bb758301cf3d70a4cc34c7dc90e3"
-bot_token = "6190621947:AAHjQJNowLtXQjLSaO8oRAY16GUlX7af_m4"
-sudo_group = -1001814153201
+bot_token = "6068991031:AAHJrxu6L5RjMq9x2vhrwuQRHDi1KUMDhR4"
+sudo_group = -1001877014544 
