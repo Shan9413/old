39681c44e8db44eb56b0e1cb743adafb8024c7b8
ChVivekTomar <125326355+ChVivekTomar@users.noreply.github.com>
2023-03-11 02:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -328,8 +328,8 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`\n\n**Bot By :-** @VivekTomarBro"
                 prog = await bot.send_message(m.chat.id, Show)
-                cc = f"**Name »** {name1} {res}.mkv\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n**Join »** @OfficialVivekTomarBro"
-                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n**Join »** @OfficialVivekTomarBro"
+                cc = f"**Name »** {name1} {res}.mkv\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Join »** @OfficialVivekTomarBro"
+                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Join »** @OfficialVivekTomarBro"
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
                 #                     try:
