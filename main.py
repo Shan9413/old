a43b40cb6d0ff37b3c3600d747648f0219e5d8ea
Shan9413 <159189394+Shan9413@users.noreply.github.com>
2024-02-10 08:51:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ import io
 #import pycurl
 
 bot = Client("bot",
-             bot_token= "6923349541:AAFc7_MEXoKYHajAKFTtMo4QzUb8KO7J82E",
-             api_id= 20088962,
-             api_hash= "257f47d347157555890a64b12bc0134f")
+             bot_token= "6746876285:AAGnUDNwDvWu4RfbkSSiQe8gt-2lU-9p-Bk",
+             api_id= 25526898,
+             api_hash= "abd3843f8151f952fbfd521c2404a8ba")
 
 
 @bot.on_message(filters.command(["start"]))
